d3313c13b188899645107869896e953c59dbf4bb
Jesper <jesper.lundqvist@gmail.com>
2020-04-25 18:42:42 +0200

    inital game logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const STARTING_TIME = 5;
+
+  const [text, setText] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(0);
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setText(value);
+    calculateWords(text);
+  };
+
+  const calculateWords = (text) => {
+    const wordsArray = text
+      .trim()
+      .split(' ')
+      .filter((word) => word !== '');
+    setWordCount(wordsArray.length);
+  };
+
+  useEffect(() => {
+    if (timeRemaining <= 0) {
+      setIsGameStarted(false)
+      return;
+    };
+
+    setTimeout(() => {
+      setTimeRemaining((time) => time - 1);
+    }, 1000);
+  }, [timeRemaining]);
+
+  const startGame = () => {
+    setIsGameStarted(true);
+    setTimeRemaining(STARTING_TIME);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <p>Word Count: {wordCount}</p>
+      <button onClick={startGame}>Start Game</button>
     </div>
   );
 }
